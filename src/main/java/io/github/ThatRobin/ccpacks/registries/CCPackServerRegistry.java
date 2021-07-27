d2437e983cef8ace8d2b4407df4a142900c61e5d
PixelNinja5467 <zakaryzk@icloud.com>
2021-07-27 09:40:30 +0100

    New updates are now server side too
    was an idiot

--- src/main/java/io/github/ThatRobin/ccpacks/registries/CCPackServerRegistry.java
+++ src/main/java/io/github/ThatRobin/ccpacks/registries/CCPackServerRegistry.java
@@ -248,7 +248,9 @@ public class CCPackServerRegistry {
                     }
                     DDBlock EXAMPLE_BLOCK = new DDBlock(blockSettings);
                     Registry.register(Registry.BLOCK, instance2.getId("identifier"), EXAMPLE_BLOCK);
-                    Registry.register(Registry.ITEM, instance2.getId("identifier"), new BlockItem(EXAMPLE_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+                    if (instance2.getBoolean("make_block_item")) {
+                        Registry.register(Registry.ITEM, instance2.getId("identifier"), new BlockItem(EXAMPLE_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+                    }
                 } else if(itemType.equals("falling")) {
                     instance2 = SerializableObjects.blockData.read(jsonObject);
 
@@ -261,8 +263,9 @@ public class CCPackServerRegistry {
                     }
                     DDFallingBlock EXAMPLE_BLOCK = new DDFallingBlock(blockSettings);
                     Registry.register(Registry.BLOCK, instance2.getId("identifier"), EXAMPLE_BLOCK);
-                    Registry.register(Registry.ITEM, instance2.getId("identifier"), new BlockItem(EXAMPLE_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
-
+                    if (instance2.getBoolean("make_block_item")) {
+                        Registry.register(Registry.ITEM, instance2.getId("identifier"), new BlockItem(EXAMPLE_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+                    }
                 } else if(itemType.equals("horizontal_slab")) {
                     instance2 = SerializableObjects.blockData.read(jsonObject);
 
@@ -275,8 +278,9 @@ public class CCPackServerRegistry {
                     }
                     DDHSlabBlock EXAMPLE_BLOCK = new DDHSlabBlock(blockSettings);
                     Registry.register(Registry.BLOCK, instance2.getId("identifier"), EXAMPLE_BLOCK);
-                    Registry.register(Registry.ITEM, instance2.getId("identifier"), new BlockItem(EXAMPLE_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
-
+                    if (instance2.getBoolean("make_block_item")) {
+                        Registry.register(Registry.ITEM, instance2.getId("identifier"), new BlockItem(EXAMPLE_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+                    }
                 } else if(itemType.equals("vertical_slab")) {
                     instance2 = SerializableObjects.blockData.read(jsonObject);
 
@@ -289,8 +293,9 @@ public class CCPackServerRegistry {
                     }
                     DDVSlabBlock EXAMPLE_BLOCK = new DDVSlabBlock(blockSettings);
                     Registry.register(Registry.BLOCK, instance2.getId("identifier"), EXAMPLE_BLOCK);
-                    Registry.register(Registry.ITEM, instance2.getId("identifier"), new BlockItem(EXAMPLE_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
-
+                    if (instance2.getBoolean("make_block_item")) {
+                        Registry.register(Registry.ITEM, instance2.getId("identifier"), new BlockItem(EXAMPLE_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+                    }
                 } else if(itemType.equals("stairs")) {
                     instance2 = SerializableObjects.stairsData.read(jsonObject);
 
@@ -304,8 +309,24 @@ public class CCPackServerRegistry {
                     }
                     DDStairBlock EXAMPLE_BLOCK = new DDStairBlock(state, blockSettings);
                     Registry.register(Registry.BLOCK, instance2.getId("identifier"), EXAMPLE_BLOCK);
-                    Registry.register(Registry.ITEM, instance2.getId("identifier"), new BlockItem(EXAMPLE_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+                    if (instance2.getBoolean("make_block_item")) {
+                        Registry.register(Registry.ITEM, instance2.getId("identifier"), new BlockItem(EXAMPLE_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+                    }
+                }else if (itemType.equals("fence")) {
+                    instance2 = SerializableObjects.blockData.read(jsonObject);
 
+                    Material mat = getMat(instance2.getString("material"));
+                    BlockSoundGroup sounds = getSound(instance2.getString("sound"));
+                    Tag<Item> tools = getTool(instance2.getString("effective_tool"));
+                    FabricBlockSettings blockSettings = FabricBlockSettings.of(mat).breakByTool(tools, instance2.getInt("mining_level")).collidable(instance2.getBoolean("collidable")).strength(instance2.getInt("hardness"), instance2.getInt("resistance")).slipperiness(instance2.getFloat("slipperiness")).luminance(instance2.getInt("luminance")).sounds(sounds).requiresTool().drops(instance2.getId("loot_table"));
+                    if (instance2.getBoolean("transparent")) {
+                        blockSettings.nonOpaque();
+                    }
+                    DDFenceBlock EXAMPLE_BLOCK = new DDFenceBlock(blockSettings);
+                    Registry.register(Registry.BLOCK, instance2.getId("identifier"), EXAMPLE_BLOCK);
+                    if (instance2.getBoolean("make_block_item")) {
+                        Registry.register(Registry.ITEM, instance2.getId("identifier"), new BlockItem(EXAMPLE_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+                    }
                 }
             } else if (type.equals("ccpacks:keybind")) {
 

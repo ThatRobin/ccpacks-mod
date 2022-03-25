235a7c2835f51b1de5144c2350061371e55073c4
Robin <36313416+ThatRobin@users.noreply.github.com>
2022-03-25 15:58:08 +0000

    Delete src/main/java/io/github/thatrobin/ccpacks/Util directory

--- src/main/java/io/github/thatrobin/ccpacks/Util/AccessFactory.java
+++
@@ -1,7 +0,0 @@
-package io.github.thatrobin.ccpacks.util;
-
-import io.github.apace100.apoli.power.factory.PowerFactory;
-
-public interface AccessFactory {
-    PowerFactory<?> getFactory();
-}
--- src/main/java/io/github/thatrobin/ccpacks/Util/IDDBlock.java
+++
@@ -1,9 +0,0 @@
-package io.github.thatrobin.ccpacks.util;
-
-import io.github.thatrobin.ccpacks.data_driven_classes.blocks.DDBlockEntity;
-import net.minecraft.block.entity.BlockEntityType;
-
-public interface IDDBlock {
-
-    void setType(BlockEntityType<DDBlockEntity> blockEntityType);
-}

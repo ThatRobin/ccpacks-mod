cea5bb2aa244ac0a8a164045db435c260df2bc7d
Robin <36313416+ThatRobin@users.noreply.github.com>
2022-03-25 15:58:01 +0000

    Delete src/main/java/io/github/thatrobin/ccpacks/util directory

--- src/main/java/io/github/thatrobin/ccpacks/util/OnLoadResourceManager.java
+++
@@ -1,8 +0,0 @@
-package io.github.thatrobin.ccpacks.util;
-
-public interface OnLoadResourceManager {
-
-    static void addSingleListener(DataLoader loader) {
-        OnLoadResourceManageImpl.addSingleListener(loader);
-    }
-}
